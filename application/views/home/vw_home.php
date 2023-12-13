eb856d07ac3699ab782e297d56bb461c5c2a4f2c
harelnainggolan98 <harel22ti@mahasiswa.pcr.ac.id>
2023-12-13 11:02:27 +0700

    home

--- application/views/home/vw_home.php
+++ application/views/home/vw_home.php
@@ -7,26 +7,38 @@
             </center>
           </div>
         </div>
-      <div class="col-sm-6 col-xl-3">
-            <div class="card overflow-hidden rounded-2">
-              <div class="position-relative">
-                <a href="javascript:void(0)"><img src="<?= base_url('assets/') ?>src/images/products/meja.jpg" class="card-img-top rounded-0" alt="..."></a>
-                <a href="javascript:void(0)" class="bg-primary rounded-circle p-2 text-white d-inline-flex position-absolute bottom-0 end-0 mb-n3 me-3" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Add To Cart"><i class="ti ti-basket fs-4"></i></a>                      </div>
-              <div class="card-body pt-3 p-4">
-                <h6 class="fw-semibold fs-4">Meja</h6>
-                <div class="d-flex align-items-center justify-content-between">
-                  <h6 class="fw-semibold fs-4 mb-0">Rp 50.000,00 <span class="ms-2 fw-normal text-muted fs-3"><del>$65</del></span></h6>
-                  <ul class="list-unstyled d-flex align-items-center mb-0">
-                    <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
-                    <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
-                    <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
-                    <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
-                    <li><a class="" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
-                  </ul>
+        <div class="row">
+        <div class="col-sm-6 col-xl-3">
+            <?php $i = 1; ?>
+            <?php foreach ($postingan as $us): ?>
+                <div class="card overflow-hidden rounded-2">
+                    <div class="position-relative">
+                        <a href="javascript:void(0)"><img
+                                src="<?= base_url('assets/src/images/products/') . $us['gambar']; ?>"
+                                class="card-img-top rounded-0" alt="..."></a>
+                        <a href="<?= base_url('index.php/Postingan/hapus/'); ?>"
+                            class="btn btn-danger position-absolute top-0 end-0 mt-3 me-3" class>Hapus</a>"
+                    </div>
+                    <div class="card-body pt-3 p-4">
+                        <h6 class="fw-semibold fs-4">
+                            <?= $us['nama']; ?>
+                        </h6>
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h6 class="fw-semibold fs-4 mb-0">
+                                <?= $us['harga']; ?>
+                                <!-- <ul class="list-unstyled d-flex align-items-center mb-0">
+                        <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
+                        <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
+                        <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
+                        <li><a class="me-1" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
+                        <li><a class="" href="javascript:void(0)"><i class="ti ti-star text-warning"></i></a></li>
+                    </ul> -->
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+        </div>
       </div>  
     </div>
   </div>
